74e5878ed718cd882b1b4c1b37ea49fcd0b8db84
Mohammad Zaid Ansari <mohdzaid88798@gmail.com>
2025-11-07 10:18:30 +0530

    Added Send Email Page

--- emaill-sender/email-frontend/src/App.tsx
+++ emaill-sender/email-frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SendEmail from "./pages/SendEmail";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +17,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/send" element={<SendEmail />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
